b5b9afd54b61ea3c76cb65981cef4a8022ad2187
Federico Russo <federico.russo@antea.tech>
2021-06-01 12:29:53 +0200

    Compiles to Java 8.

--- src/main/java/com/anteash/gzipcompressor/GzipCompressorMojo.java
+++ src/main/java/com/anteash/gzipcompressor/GzipCompressorMojo.java
@@ -27,18 +27,12 @@ public class GzipCompressorMojo extends MojoSupport {
         if (".gz".equalsIgnoreCase(FileUtils.getExtension(file.getName()))) {
             return;
         }
-        
+
         final File gzipped = new File(file.getAbsolutePath() + ".gz");
         getLog().debug(String.format("Compressing %s to %s", file.getName(), gzipped.getName()));
-        GZIPOutputStream out = null;
-        FileInputStream in = null;
-        try {
-            out = new GZIPOutputStream(new FileOutputStream(gzipped));
-            in = new FileInputStream(file);
+        try (GZIPOutputStream out = new GZIPOutputStream(new FileOutputStream(gzipped));
+             FileInputStream in = new FileInputStream(file)) {
             IOUtil.copy(in, out);
-        } finally {
-            IOUtil.close(in);
-            IOUtil.close(out);
         }
     }
 }
